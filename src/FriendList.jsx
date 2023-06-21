12a3ca13f634db20bcaa99268859218b258a622d
wangseong <dladhkdtjd@naver.com>
2023-06-21 19:49:53 +0900

    first

---
+++ src/FriendList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ScrollView, View } from "react-native";
+import Margin from "./Margin";
+import Profile from "./Profile";
+
+export default (props) => {
+  return (
+    props.isOpened && (
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {props.data.map((item, index) => (
+          <View key={index}>
+            <Profile
+              uri={item.uri}
+              name={item.name}
+              introduction={item.introduction}
+            />
+            <Margin height={13} />
+          </View>
+        ))}
+      </ScrollView>
+    )
+  );
+};
---
+++ src/Margin.jsx
@@ -0,0 +1,5 @@
+import { View, Text } from "react-native";
+
+export default function Margin(props) {
+  return <View style={{ height: props.height }}></View>;
+}
